a5de580852c2b4fa2435259ad661115e3ce36382
httpLucasReis <manoel.reisiema@gmail.com>
2020-11-28 01:28:29 -0300

    Finishing CRUD of users

--- src/routes.js
+++ src/routes.js
@@ -4,5 +4,8 @@ const routes = express.Router();
 
 // Creating routes
 routes.get('/users', userController.index);
+routes.post('/users/', userController.create);
+routes.put('/users/:id', userController.update);
+routes.delete('/users/:id', userController.delete);
 
 module.exports = routes;
--- src/server.js
+++ src/server.js
@@ -2,7 +2,22 @@ const express = require('express');
 const app = express();
 const routes = require('./routes');
 
+app.use(express.json());
 app.use(routes);
+
+// notFound
+app.use((req, res, next) => {
+  const error = new Error;
+  error.status = 404;
+  error.message = 'Not Found';
+  next(error);
+})
+
+// catch all
+app.use((error, req, res, next) => {
+  res.status(error.status || 500);
+  res.json({ error: error.message })
+})
 
 app.listen(3333, ()=> {
   console.log('Server is running');
